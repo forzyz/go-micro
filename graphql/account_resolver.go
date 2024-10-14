623b5663e9cf9be9e6e17ad76582162dcca3c0f2
Ruslan Telitsyn <zimpuytel@gmail.com>
2024-10-14 15:58:03 +0300

    setting up + graphql service

---
+++ graphql/account_resolver.go
@@ -0,0 +1,9 @@
+package main
+
+type accountResolver struct {
+	server *Server
+}
+
+// func (r *queryResolver) Orders(ctx context.Context, obj *Account) ([]*Order, error) {
+
+// }
---
+++ graphql/mutation_resolver.go
@@ -0,0 +1,17 @@
+package main
+
+type mutationResolver struct {
+	server *Server
+}
+
+// func (r *mutationResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
+// 	return
+// }
+
+// func (r *mutationResolver) createAccount(ctx context.Context, in ProductInput) (*Product, error) {
+// 	return
+// }
+
+// func (r *mutationResolver) createAccount(ctx context.Context, in OrderInput) (*Order, error) {
+// 	return
+// }
